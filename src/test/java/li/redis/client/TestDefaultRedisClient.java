780842f4886781fcba7160697c8014f313abbdf3
李广 <ligbit@163.com>
2020-07-22 20:46:53 +0800

    补充多线程测试用例

--- src/test/java/li/redis/client/TestDefaultRedisClient.java
+++ src/test/java/li/redis/client/TestDefaultRedisClient.java
@@ -3,17 +3,23 @@ package li.redis.client;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class TestDefaultRedisClient {
 
     private DefaultRedisClient client;
 
     @Before
-    public void init(){
-         client = new DefaultRedisClient("localhost", 6379);
+    public void init() {
+        client = new DefaultRedisClient("localhost", 6379);
     }
 
     @Test
-    public void testSet(){
+    public void testSet() {
         String set = client.set("city", "shenzhen");
         System.out.println(set);
     }
@@ -28,6 +34,23 @@ public class TestDefaultRedisClient {
     public void testDel(){
         boolean del = client.del("city");
         System.out.println(del);
+    }
+
+    // todo 这种方式存在多线程问题，会发生粘包，可能一次接收到多个结果，需要修改实现方式
+    @Test
+    public void testMultiThreadSet() {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Map<String,String> map = new ConcurrentHashMap<>();
+        for (int i = 0; i < 10000; i++) {
+            executorService.submit(() -> map.put(client.set("foo","bar"),""));
+        }
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 正常来说，假如客户端实现方式是线程安全的，那么这里只会存在一个key,即：+OK
+        System.out.println(map.keySet());
     }
 
 
